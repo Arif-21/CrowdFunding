e594067f0fc5e6973287213d270d6bfba6d8a8c2
Arif-21 <arif.murdiyono2107@gmail.com>
2020-12-31 18:18:17 +0700

    Tugas Quiz 3 - first

---
+++ resources/js/App.vue
@@ -0,0 +1,141 @@
+<template>
+    <v-app>
+        <!-- sidebar -->
+        <v-navigation-drawer app v-model="drawer"
+            dark
+            src="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
+        >
+            <v-list>
+                <v-list-item v-if="!guest">
+                    <v-list-item-avatar>
+                        <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+                    </v-list-item-avatar>
+                    <v-list-item-content>
+                        <v-list-item-title>John Leider</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item> 
+
+                <div class="pa-2" v-if="guest">
+                    <v-btn block color="primary" class="mb-1">
+                        <v-icon left>mdi-lock</v-icon>
+                        Login
+                    </v-btn>
+                    <v-btn block color="success" class="mb-1">
+                        <v-icon left>mdi-account</v-icon>
+                        Register
+                    </v-btn>
+                </div>
+
+                <v-divider></v-divider>
+
+                <v-list-item
+                    v-for="(item, index) in menus"
+                    :key="`menu-`+index"
+                    :to="item.route"
+                >
+                    <v-list-item-icon>
+                        <v-icon left>{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+
+            <template v-slot:append v-if="!guest">
+                <div class="pa-2">
+                    <v-btn block color="red" dark>
+                    <v-icon left>mdi-icon</v-icon>
+                        Logout
+                    </v-btn>
+                </div>
+            </template>
+        </v-navigation-drawer>
+
+        <!-- header -->
+        <v-app-bar app color="blue darken-3" dark v-if="isHome">
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+            <v-toolbar-title>SanbercodeApp</v-toolbar-title>
+
+            <!-- pemisah content -->
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+                <v-badge color="orange" overlap>
+                    <template v-slot:badge >
+                        <span>{{ $store.state.count }}</span>
+                    </template>
+                    <v-icon>mdi-cards</v-icon>
+                </v-badge>
+            </v-btn>               
+            <!-- search -->
+            <v-text-field
+                slot="extension"
+                hide-details
+                append-icon="mdi-microphone"
+                flat
+                label="Search"
+                prepend-inner-icon="mdi-magnify"
+                solo-inverted
+            ></v-text-field>
+        </v-app-bar>
+        <!-- header 2 -->
+        <v-app-bar app color="blue darken-3" dark v-else>
+            <v-btn icon @click.stop="$router.go(-1)">
+                <v-icon>mdi-arrow-left-circle</v-icon>
+            </v-btn>
+            
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+                <v-badge color="orange" overlap>
+                    <template v-slot:badge >
+                        <span>{{ $store.state.count }}</span>
+                    </template>
+                    <v-icon>mdi-cards</v-icon>
+                </v-badge>
+            </v-btn>    
+        </v-app-bar>
+
+        <!-- main -->
+        <v-main>
+
+            <!-- Provides the application the proper gutter -->
+            <v-container fluid>
+
+                <!-- If using vue-router -->
+                <v-slide-y-transition>
+                    <router-view></router-view>
+                </v-slide-y-transition>
+            </v-container>
+        </v-main>
+
+        <!-- footer -->
+        <v-card>
+            <v-footer absolute app>
+                <v-card-text class="text-center">
+                    &copy; {{ new Date().getFullYear() }} - <strong>SanbercodeApp</strong>
+                </v-card-text>
+            </v-footer>
+        </v-card>
+    </v-app>
+</template>
+<script>
+    export default {
+        name: 'App',
+        data: () => ({
+            drawer: false,
+            menus: [
+                {title: 'Home', icon: 'mdi-home', route: '/'},
+                {title: 'Campaigns', icon: 'mdi-hand-heart', route: '/campaigns'},
+            ],
+            guest: false,
+        }),
+        computed: {
+            isHome () {
+                return (this.$route.path === '/' || this.$route.path === '/home')
+            },
+        }
+    }
+</script>
---
+++ resources/js/views/Campaign.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+        <v-card v-if="campaign.id">
+            <v-img
+                :src="campaign.image"
+                class="white--text"
+                height="200px"
+            >
+                <v-card-title
+                    class="fill-height align-end"
+                    v-text="campaign.title"
+                >
+                </v-card-title>
+            </v-img>
+            
+            <v-card-text>
+                <v-simple-table dense>
+                    <tbody>
+                        <tr>
+                            <td><v-icon>mdi-map-marker</v-icon> Address</td>
+                            <td>{{ campaign.address }}</td>
+                        </tr>
+                        <tr>
+                            <td><v-icon>mdi-hand-heart</v-icon> Collect</td>
+                            <td class="blue--text">Rp {{ campaign.collected.toLocaleString('id-ID') }}</td>
+                        </tr>
+                        <tr>
+                            <td><v-icon>mdi-cash-multiple</v-icon> Required</td>
+                            <td class="blue--text">Rp {{ campaign.required.toLocaleString('id-ID') }}</td>
+                        </tr>
+                    </tbody>
+                </v-simple-table>
+                Description: 
+                <br>
+                <br>
+                {{ campaign.description }}    
+
+            </v-card-text>
+            <v-card-actions>
+                <v-btn block color="primary" @click="donate" :disabled="campaign.collected >= campaign.required"> 
+                    <v-icon>mdi-money</v-icon> &nbsp;
+                    DONATE
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </div>
+</template>
+<script>
+    export default {
+        data: () => ({
+            campaign: {},
+        }),
+        created() {
+            this.go()   
+        },
+        methods: {
+            go() {
+                let { id } = this.$route.params
+                let url = '/api/campaign/'+id
+                axios.get(url)
+                .then((response) => {
+                    let { data } = response.data
+                    this.campaign = data.campaign
+                })
+                .catch((error) => {
+                    let { responses } = error
+                    console.log(responses)
+                })
+            },
+            donate() {
+                this.$store.commit("increment")
+            }
+        },
+    }
+</script>
